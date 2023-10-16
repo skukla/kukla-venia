4fc805041de5784c5e02d47f5b97117da664ce97
Steve Kukla <kukla@adobe.com>
2023-10-16 12:04:23 -0400

    Reverted theme vendor back to MagentoEse

--- media/theme/Kukla/venia_datapack/registration.php
+++
@@ -1,3 +0,0 @@
-<?php
-use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Kukla/venia_datapack', __DIR__);
---
+++ media/theme/MagentoEse/venia_datapack/registration.php
@@ -0,0 +1,3 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/MagentoEse/venia_datapack', __DIR__);
